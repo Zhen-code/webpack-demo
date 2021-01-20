b307040648bf2fb3640567c441ff9b141f4ef581
zhen-code <945003895@qq.com>
2021-01-20 23:33:20 +0800

    路径重写与dev-server配置

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import './src/font/iconfont.css';
-import './src/css/index.css'; // eslint-disable-next-line
+import '@css/index.css'; // eslint-disable-next-line
 import $ from 'jquery'
-import print from './src/js/print';
+// eslint-disable-next-line
+import('./src/js/print').then(res=>{
+  // eslint-disable-next-line
+  console.log(res)
+  // eslint-disable-next-line
+})
 // eslint-disable-next-line
 //实现代码分割到不同对应文件
 // import(/* webpackChunkName: "print1" */ './src/js/print1')
